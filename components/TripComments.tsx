d382e860746fce3b6cc26fa3ffa956e3da7d6f8a
Fero Hriadel <ferdinand.hriadel@gmail.com>
2024-10-03 17:15:17 +0200

    getTrips (no pagination yet)

--- components/TripComments.tsx
+++ components/TripComments.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from '@/components/Container';
 import ContentSectionHeader from '../components/ContentSectionHeader';
 import InputComment from './InputComment';
@@ -9,7 +9,7 @@ import { Trip, Comment } from '@/types';
 import { uploadImage } from '@/utils/imageUpload';
 import { useAuth } from '@/context/authContext';
 import { useToast } from '@/context/toastContext';
-import { apiCalls } from '@/utils/apiCalls';
+import { apiCalls, uriEncodeObj } from '@/utils/apiCalls';
 import { scrollToElement } from '@/utils/DOM';
 
 
@@ -20,21 +20,19 @@ interface Props {
 
 
 
-const sampleComments: Comment[] = [
-  {id: 'snsklnfl3pinmdf03c', by: 'ferdinand.hriadel@gmail.com', body: 'This is a sample comment', createdAt: new Date().toISOString(), trip: 'dkd3ddoekjfpewmfpe', image: 'https://tripia-devimages-bucket-ioioioi.s3.us-east-1.amazonaws.com/2024-08NT-NaChabenci.jpg92836.png'},
-  {id: 'snsklnfl3pinmdf4,ot94c', by: 'emaletester0@gmail.com', body: 'This is a sample comment efoepfmp fmefmewkfmewk', createdAt: new Date().toISOString(), trip: 'dkd3ddoekjfpewmfpe', image: ''},
-]
-
-const hint = 'Write something...'	
+const hint = 'Write something...';
+const pageSize = 3;
 
 
 
 const TripComments = ({ trip }: Props) => {
   const [comments, setComments] = React.useState<Comment[]>([]);
+  const [lastEvaluatedKey, setLastEvaluatedKey] = React.useState<{[key: string]: any} | null>(null);
   const [comment, setComment] = React.useState(hint);
   const [preview, setPreview] = React.useState('');
   const [fileName, setFileName] = React.useState('');
   const [isUploading, setIsUploading] = React.useState(false);
+  const [loadingComments, setLoadingComments] = useState(true);
   const { user } = useAuth();
   const { showToast } = useToast();
 
@@ -71,7 +69,7 @@ const TripComments = ({ trip }: Props) => {
   function handleSuccess(savedComment: Comment) {
     setIsUploading(false);
     setComments([savedComment, ...comments]);
-    setComment('Write something...');
+    setComment(hint);
     setPreview('');
     setFileName('');
     setTimeout(() => scrollToElement(savedComment.id), 250)
@@ -85,12 +83,39 @@ const TripComments = ({ trip }: Props) => {
     handleSuccess(saveCommentRes);
   }
 
+  async function fetchCommentsFromAPI() {
+    const encodedLastEvaluatedKey = lastEvaluatedKey ? uriEncodeObj(lastEvaluatedKey) : null;
+    const queryString = encodedLastEvaluatedKey ? `?lastEvaluatedKey=${encodedLastEvaluatedKey}&pageSize=${pageSize}&tripId=${trip.id}` : `?pageSize=${pageSize}&tripId=${trip.id}`;
+    const res = await apiCalls.get('/comments' + queryString);
+    return res;
+  }
+
+  async function loadMoreComments() {
+    setLoadingComments(true);
+    const res = await fetchCommentsFromAPI();
+    if (!res.items) { showToast(res.error || 'Failed to get more trips'); return setLoadingComments(false) }
+    setLastEvaluatedKey(res.lastEvaluatedKey);
+    setComments([...comments, ...res.items]);
+    setLoadingComments(false);
+  }
+
+  function isMoreCommentsToLoad() {
+    if (lastEvaluatedKey) return true
+    else return false;
+  }
+
+  //pick up...
+
+
+  useEffect(() => { loadMoreComments(); }, []); //fetch a couple of comments initially
+
 
   return (
     <Container className='px-4 mt-10'>
       <ContentSectionHeader text='Comments' style={{lineHeight: '2rem', fontSize: '2rem', textAlign: 'left'}} className='mb-2' />
       <InputComment onChange={onChange} comment={comment} preview={preview} onSubmit={onSubmit} loading={isUploading} />
       {comments.map(comment => ( <CommentCard comment={comment} key={comment.id} /> ))}
+      {loadingComments && <p className='my-4'>Loading...</p>}
     </Container>
   )
 }
