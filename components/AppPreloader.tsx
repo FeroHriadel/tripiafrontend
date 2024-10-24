60490f431b8cbfa61622ba448187c5b1d1eb5f33
Fero Hriadel <ferdinand.hriadel@gmail.com>
2024-10-24 11:23:03 +0200

    AppPreloader delets nonexisting groups from User.groups

--- components/AppPreloader.tsx
+++ components/AppPreloader.tsx
@@ -8,10 +8,12 @@ import { setFavoriteTrips } from "@/redux/slices/favoriteTripsSlice";
 import { setInvitations } from "@/redux/slices/invitationsSlice";
 import { setProfile } from "@/redux/slices/profileSlice";
 import { useAuth } from "@/context/authContext";
+import { Group, UserProfile } from "@/types";
 
 
 
 export const dynamic = 'force-dynamic';
+
 
 
 const checkInvitesInterval = 300000; //5min
@@ -21,6 +23,7 @@ const checkInvitesInterval = 300000; //5min
 const AppPreloader = () => {
   const { user } = useAuth(); const { email } = user;
   const invitations = useAppSelector((state) => state.invitations);
+  const profile = useAppSelector((state) => state.profile);
   const dispatch = useAppDispatch();
   
 
@@ -46,8 +49,39 @@ const AppPreloader = () => {
   async function preloadProfile() {
     const res = await apiCalls.post('/users', {email: user.email});
     if (res.error) return console.log(res.error);
-    else dispatch(setProfile(res));
+    else { dispatch(setProfile(res)); await removeNonexistingGroups(res.groups); }
   };
+
+  async function removeNonexistingGroups(profileGroups: string[]) {
+    /******************************************************************************************************************************
+      - Because of the tons of fun dynamoDB gives you during the development process:
+      - The simplest way to remove from User.groups in DB any groups that were deleted from GroupsTable is from the FE ¯\(ツ)/¯
+      - That's why this is handled here and not in the BE where it belongs
+      - Did I say I will never voluntarily work with DynamoDb again?
+    *******************************************************************************************************************************/
+    const existingGroupsIds = await getGroupsFromProfile(profileGroups); //this should only return existing groups
+    if (!existingGroupsIds) return; 
+    const nonexistingGroupsIds = getNonexistingGroups(profileGroups, existingGroupsIds);
+    if (nonexistingGroupsIds.length > 0) await updateProfileGroups(existingGroupsIds);
+  }
+
+  async function getGroupsFromProfile(groupIds: string[]) {
+    if (groupIds.length === 0) return;
+    const res = await apiCalls.post('/groupsbatchget', {ids: groupIds}); //this should only return existing groups
+    if (res.error) { console.log('Could not batchgetgroups deleted groups to remove them from user profile: ', res.error); return null };
+    return (res as Group[]).map(group => group.id);
+  }
+
+  function getNonexistingGroups(profileGroups: string[], existingGroups: string[]): string[] {
+    return profileGroups.filter(group => !existingGroups.includes(group));
+  }
+  
+  async function updateProfileGroups(existingGroupIds: string[]) {
+    const body: UserProfile = {...profile, groups: existingGroupIds};
+    const res = await apiCalls.put('/users', body);
+    if (res.error) return console.log('Failed to remove nonexisting groups from user profile', res.error);
+    dispatch(setProfile(res));
+  }
 
 
   useEffect(() => {preloadCategories();}, []); //get categories
@@ -63,7 +97,6 @@ const AppPreloader = () => {
   }, [email, invitations]);
 
   useEffect(() => {if (user.email) preloadProfile();}, [user]); //get user's profile
-
 
 
   return (<></>);
